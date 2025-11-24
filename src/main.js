3f6b5848b029201fff7a4320985f46966d476e9c
RostSuslov <suslovros@gmail.com>
2025-11-24 21:20:15 +0200

    feat: header mobile done

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import './style.css'
 
 const burger = document.querySelector('#burger')
 const mobileContainer = document.querySelector('#mobile-container')
+const body =document.querySelector('body')
 
 burger.addEventListener('click', ()=> {
     burger.classList.toggle('is-active')
     mobileContainer.classList.toggle('hidden')
+    body.classList.toggle('overflow-hidden')
 })
