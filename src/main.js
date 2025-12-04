55f3b30eb631c0846067ce356f9ff627b00389af
RostSuslov <suslovros@gmail.com>
2025-12-04 21:33:24 +0200

    feat: create feedback-button layout

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const burger = document.querySelector('#burger')
 const mobileContainer = document.querySelector('#mobile-container')
 const body = document.querySelector('body')
 
-burger.addEventListener('click', () => {
+burger.addEventListener('click', function() {
     burger.classList.toggle('is-active')
     mobileContainer.classList.toggle('hidden')
     body.classList.toggle('overflow-hidden')
@@ -27,3 +27,24 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+
+// Demo by http://creative-punch.net
+
+const items = document.querySelectorAll('.circle a');
+
+for(var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI + 1*(0.5/l)*i*Math.PI)).toFixed(4) + "%";
+  items[i].style.bottom = (50 + 35*Math.sin(-0.5 * Math.PI + 1*(0.5/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+// document.querySelector('.menu-button').onclick = function(e) {
+//    e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+// }
+
+
+const feedbackButton = document.querySelector('#feedback-button')
+
+feedbackButton.addEventListener('click', function() {
+    feedbackButton.classList.toggle('active')
+})
